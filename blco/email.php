6255b1cc366a0cd15efae402579e8a74d1e136c5
Joareliz (Joa) Pena Martinez <119870846+joareliz05@users.noreply.github.com>
2023-10-23 19:36:25 -0500

    Uploading file to repository
    Uploading file name 'blco' to repository.

---
+++ blco/email.php
@@ -0,0 +1,19 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST["name"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $message = $_POST["message"];
+
+    $to = "bucketlistjourneys@hotmail.com";
+    $headers = "From: $email" . "\r\n" .
+               "Reply-To: $email" . "\r\n" .
+               "X-Mailer: PHP/" . phpversion();
+
+    if (mail($to, $subject, $message, $headers)) {
+        echo "Email sent successfully!";
+    } else {
+        echo "Error sending email.";
+    }
+}
+?>
